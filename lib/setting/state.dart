6d4a00fa9f0e0a0a985024bd01c022f4ab7bb18c
yunt <yuntuo19900228@126.com>
2019-05-06 08:39:49 +0800

    增加登录、设置 、启动页

---
+++ lib/setting/state.dart
@@ -0,0 +1,12 @@
+import 'package:fish_redux/fish_redux.dart';
+
+class SettingState implements Cloneable<SettingState> {
+  @override
+  SettingState clone() {
+    return SettingState();
+  }
+}
+
+SettingState initState(Map<String, dynamic> args) {
+  return SettingState();
+}
