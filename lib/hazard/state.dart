5b2d5bcdc7bf1cf3aacfc80a72df5309d9762c2d
yunt <yuntuo19900228@126.com>
2019-05-08 15:39:39 +0800

    优化

--- lib/hazard/state.dart
+++ lib/hazard/state.dart
@@ -1,15 +1,20 @@
 import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart';
 import 'package:inspection/entity/hazard_model.dart';
 
 class HazardState implements Cloneable<HazardState> {
   List<HazardModel> list = List<HazardModel>();
-
+  GlobalKey<ScaffoldState> scaffoldKey;
   @override
   HazardState clone() {
-    return HazardState()..list = list;
+    return HazardState()
+      ..list = list
+      ..scaffoldKey = scaffoldKey;
   }
 }
 
 HazardState initState(Map<String, dynamic> args) {
-  return HazardState();
+  HazardState state = HazardState();
+  state.scaffoldKey = GlobalKey<ScaffoldState>();
+  return state;
 }
--- lib/hazard/view.dart
+++ lib/hazard/view.dart
@@ -7,6 +7,7 @@ Widget buildView(
     HazardState state, Dispatch dispatch, ViewService viewService) {
   ListAdapter listAdapter = viewService.buildAdapter();
   return Scaffold(
+    key: state.scaffoldKey,
     appBar: AppBar(
       title: Text(
         '隐患记录',
@@ -14,11 +15,29 @@ Widget buildView(
       ),
       centerTitle: true,
       backgroundColor: Colors.white,
+      actions: <Widget>[
+        GestureDetector(
+          onTap: () {
+            state.scaffoldKey.currentState.openEndDrawer();
+          },
+          child: Container(
+            padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+            alignment: AlignmentDirectional.center,
+            child: Text(
+              '查询',
+              style: TextStyle(color: Colors.black, fontSize: 15.0),
+            ),
+          ),
+        )
+      ],
     ),
     body: ListView.builder(
       padding: EdgeInsets.only(bottom: 50.0),
       itemBuilder: listAdapter.itemBuilder,
       itemCount: listAdapter.itemCount,
+    ),
+    endDrawer: Drawer(
+      child: Container(),
     ),
   );
 }
