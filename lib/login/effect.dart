be37aa04ff269051286354c0371dbe29d682c5af
yunt <yuntuo19900228@126.com>
2019-05-28 11:06:44 +0800

    页面优化

--- lib/login/effect.dart
+++ lib/login/effect.dart
@@ -18,29 +18,28 @@ Effect<LoginState> buildEffect() {
 void _onAction(Action action, Context<LoginState> ctx) {}
 
 void _onLogin(Action action, Context<LoginState> ctx) {
-//  if (ctx.state.userNameController.text.isEmpty) {
-//    AppToast.showToast('用户名不能为空');
-//  } else if (ctx.state.passwordController.text.isEmpty) {
-//    AppToast.showToast('密码不能空');
-//  } else {
-  Map<String, String> map = Map();
-  map['username'] = 'system';
-  map['password'] = '111111';
-  DicoHttpRepository.userLogin(map).then((model) {
-    if (model.code == 0) {
-      AppCommons.showToast(model.msg);
-      SpUtils sp = SpUtils();
-      sp.putString('token', 'Bearer' + ' ${model.data.token}');
-      sp.putString('name', '${model.data.name}');
-      sp.putString('company', '${model.data.company}');
-      sp.putString('avatar', '${model.data.icon}');
-      sp.putInt('isLoginOk', 1);
-      Navigator.of(ctx.context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (context) => IndexPage().buildPage(null)),
-          (route) => route == null);
-    } else {
-      AppCommons.showToast(model.msg);
-    }
-  });
-//  }
+  if (ctx.state.userNameController.text.isEmpty) {
+    AppCommons.showToast('用户名不能为空');
+  } else if (ctx.state.passwordController.text.isEmpty) {
+    AppCommons.showToast('密码不能空');
+  } else {
+    Map<String, String> map = Map();
+    DicoHttpRepository.userLogin(map).then((model) {
+      if (model.code == 0) {
+        AppCommons.showToast(model.msg);
+        SpUtils sp = SpUtils();
+        sp.putString('token', 'Bearer' + ' ${model.data.token}');
+        sp.putString('name', '${model.data.name}');
+        sp.putString('company', '${model.data.company}');
+        sp.putString('avatar', '${model.data.icon}');
+        sp.putInt('isLoginOk', 1);
+        Navigator.of(ctx.context).pushAndRemoveUntil(
+            MaterialPageRoute(
+                builder: (context) => IndexPage().buildPage(null)),
+            (route) => route == null);
+      } else {
+        AppCommons.showToast(model.msg);
+      }
+    });
+  }
 }
